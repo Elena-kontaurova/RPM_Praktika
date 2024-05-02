9047192ffbed3e45e03a3b092032d07d508c5566
Elena Kontaurova <kontaurova0306@gmail.com>
2024-05-02 16:17:00 +0300

    Обновление описания методов

--- main.py
+++ main.py
@@ -54,13 +54,18 @@ async def set_name_by_anketa_handler(msg: Message, state: FSMContext):
 
 @router.callback_query(F.data == 'set_name_anketa')
 async def set_name_anketa_handler(callback_query: CallbackQuery, state: FSMContext):
-    ''' Метод для '''
+    ''' Метод используется для обработки нажатия пользователем кнопки 
+"Установить имя". При нажатии на данную кнопку, будет вызван метод, который 
+поможет пользователю установить свое имя в анкете и перецти дальше. '''
     await anketa_handler(callback_query.message, state)
 
 
 @router.message(Anketa.age)
 async def set_age_by_anketa_handler(msg: Message, state: FSMContext):
-    ''' Метод для ввода'''
+    ''' Метод используется для обработки ввода возраста пользоватлелем. 
+Если возраст будет введен некорректно, пользователю будет отправлено 
+сообщение об ошибке и предложит ввести возраст снова. После успешного 
+ввода возраста, будет предложенно ввести пол. '''
     try:
         await state.update_data(age=int(msg.text))
     except ValueError:
@@ -81,7 +86,9 @@ async def set_age_by_anketa_handler(msg: Message, state: FSMContext):
 
 @router.callback_query(F.data == 'set_age_anketa')
 async def set_age_anketa_handler(callback_query: CallbackQuery, state: FSMContext):
-    ''' Метод для '''
+    ''' Метод - обработчик callback-запросов. Используется для 
+перехода к установке возраста, при заполнении анкеты. Также есть 
+кнопки "Назад" и "Отмена". Метод обеспечивает удобное упровление процессами. '''
     await state.set_state(Anketa.age)
     markup = InlineKeyboardMarkup(inline_keyboard=[[
         InlineKeyboardButton(text = 'Назад', callback_data='set_name_anketa'),
@@ -91,7 +98,11 @@ async def set_age_anketa_handler(callback_query: CallbackQuery, state: FSMContex
 
 @router.message(Anketa.gender)
 async def set_age_by_anketa_two_handler(msg: Message, state: FSMContext):
-    ''' Метод для '''
+    ''' Метод - обработчик сообщений. Используется для установки 
+пола. При получении сообщения от пользователя с данными о поле, 
+метод обнавляет данные состояния, записывая в них значения поля gender. 
+Затем метод отправляет сообщение пользователю со строковым представлением данных. 
+И в концен, метод очищает состояние, для подготовке его к новому запросу. '''
     await state.update_data(gender=msg.text)
     await msg.answer(str(await state.get_data()))
     await state.clear()
@@ -99,7 +110,10 @@ async def set_age_by_anketa_two_handler(msg: Message, state: FSMContext):
 
 @router.message(Command('start'))
 async def start_handler(msg: Message):
-    ''' Метод для '''
+    ''' Метод используется для обработки команды start. Когда пользователь 
+отправляет эту команду, бот отправляет список команд, которые можно использовать 
+в чате, также предлогает перейти на другую страницу. Кнопка "Вперед", 
+которая при нажатии отправит на callback_data 'next'. '''
     await bot.set_my_commands([
         BotCommand(command='start', description='Запуск бота'),
         BotCommand(command='anketa', description='Справка'),
@@ -115,7 +129,10 @@ async def start_handler(msg: Message):
 
 @router.callback_query(F.data == 'next')
 async def next_handler(callback_query: CallbackQuery):
-    ''' Метод для '''
+    ''' Метод - обрабатыевает next. Когда пользователь нажимает "Вперед" 
+на предыдущей странице, метод страбатывает. Внутри метода, стоздается объект
+с кнопкой "Назад",которая будет отображатся на следующей странице. 
+Происходит изменение теста на "Страница 2", и обновление клавиатуры. '''
     inline_markup = InlineKeyboardMarkup(inline_keyboard=[
         [InlineKeyboardButton(text='Назад', callback_data='back')]
     ])
@@ -126,7 +143,9 @@ async def next_handler(callback_query: CallbackQuery):
 
 @router.callback_query(F.data == 'back')
 async def back_handler(callback_query: CallbackQuery):
-    ''' Метод для '''
+    ''' Метод для обработки нажатия кномпи "Назад". 
+Метод удаляет текущее сообщение, на которое пользователь нажал "Назад", 
+и отправляет новое сообщение с текстом "Страница 1", и кнопкой "Впредед". '''
     inline_markup = InlineKeyboardMarkup(inline_keyboard=[
         [InlineKeyboardButton(text='Вперед', callback_data='next')]
     ])
@@ -137,7 +156,8 @@ async def back_handler(callback_query: CallbackQuery):
 
 
 async def main():
-    ''' Что то'''
+    ''' Метод запускает процесс опроса, входящих обнов для бота. 
+Основаная точка входа и запуска бота. '''
     await dp.start_polling(bot)
 
 
